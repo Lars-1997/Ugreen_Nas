b788febc835bbd3c0c9d040b95882807ae3d601d
Lars <lars.t1997@gmail.com>
2025-08-03 20:54:04 +0200

    Add_ignore_option_for_folders

--- Skripts/functions/list_operations.py
+++ Skripts/functions/list_operations.py
@@ -1,3 +1,8 @@
+import sys
+sys.path.append("./.")
+from skripts.parameters import get_ignore_file_names
+
+
 def remove_folders (list: list) -> list:
     """
     Removes folder paths from a list of file paths and returns only file names.
@@ -35,3 +40,26 @@ def remove_existing_files_from_list_with_folders(list_to_remove: list, list_with
         list: A list of file paths from list_to_remove that do not have corresponding file names in list_with_movable_files.
     """
     return [file for file in list_to_remove if file.split("\\")[-1] in list_with_movable_files]
+
+def remove_unwanted_folders_from_list(list_to_remove_from: list, list_of_folders_to_remove: list) -> list:
+    """
+    Removes folders from list_to_remove_from that are present in list_of_folders_to_remove.
+    Args:
+        list_to_remove_from (list): List of file paths to be checked if they exist in the list_of_folders_to_remove.
+        list_of_folders_to_remove (list): List of folder names to check against if they exist in the first list.
+    Returns:
+        list: A list of file paths from list_to_remove_from that do not have corresponding folder names in list_of_folders_to_remove.
+    """
+    for entry in list_to_remove_from:
+        for folder in list_of_folders_to_remove:
+            if folder in entry:
+                list_to_remove_from.remove(entry)
+                break
+
+    ignore_file_names = get_ignore_file_names()
+    for entry in list_to_remove_from:
+        for names in ignore_file_names:
+            if names in entry:
+                list_to_remove_from.remove(entry)
+                break
+    return list_to_remove_from
---
+++ Skripts/parameters.py
@@ -0,0 +1,13 @@
+ignore_file_names = [
+        'ignore_folders.txt',
+        'ignore_folders_list.txt',
+        'ignore_folders.txt',
+        'ignorieren.txt',
+        'to_be_ignored.txt',
+    ]
+
+def get_ignore_file_names() -> list:
+    """
+    Returns a list of possible names for the ignore file.
+    """
+    return ignore_file_names
