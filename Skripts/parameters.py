a720ce42f82a77891b927d2d36e2bf63a9c8acaa
Lars <lars.t1997@gmail.com>
2025-08-15 19:50:50 +0200

    this_should_work

--- Skripts/parameters.py
+++ Skripts/parameters.py
@@ -1,10 +1,11 @@
 ignore_file_names = [
-        'ignore_folders.txt',
-        'ignore_folders_list.txt',
-        'ignore_folders.txt',
-        'ignorieren.txt',
-        'to_be_ignored.txt',
-    ]
+    "ignore_folders.txt",
+    "ignore_folders_list.txt",
+    "ignore_folders.txt",
+    "ignorieren.txt",
+    "to_be_ignored.txt",
+]
+
 
 def get_ignore_file_names() -> list:
     """
