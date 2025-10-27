895ad481830d4c4c24c826eb87b79720d87d7572
Lars <lars.t1997@gmail.com>
2025-10-27 15:34:43 +0100

    Add_a_heic_to_jpg_script

---
+++ Heic_to_jpeg/main.py
@@ -0,0 +1,66 @@
+#####################################################
+###                 Import Modules                ###
+#####################################################
+import os
+from PIL import Image
+from pillow_heif import register_heif_opener
+
+
+######################################################
+###               Create Functions                 ###
+######################################################
+def create_list_of_heic_files(source_directory):
+    """
+    Creates a list of all .heic files in the specified source directory and its subdirectories.
+    Args:
+        source_directory (str): The path to the source directory.
+    Returns:
+        list: A list of file paths for all .heic files found.
+    """
+    heic_files = []
+    for root, dirs, files in os.walk(source_directory):
+        for file in files:
+            if file.lower().endswith(".heic"):
+                heic_files.append(os.path.join(root, file))
+    return heic_files
+
+
+def convert_heic_to_jpeg(heic_file_path, output_directory):
+    """
+    Converts a .heic file to .jpeg format and saves it in the specified output directory.
+    Args:
+        heic_file_path (str): The path to the .heic file.
+        output_directory (str): The directory where the converted .jpeg file will be saved.
+    Returns:
+        str: The path to the converted .jpeg file.
+    """
+    register_heif_opener()
+    image = Image.open(heic_file_path)
+    jpeg_file_name = os.path.splitext(os.path.basename(heic_file_path))[0] + ".jpeg"
+    jpeg_file_path = os.path.join(output_directory, jpeg_file_name)
+    image.convert("RGB").save(jpeg_file_path, "JPEG")
+    return jpeg_file_path
+
+
+def remove_unwanted_heic_files_from_directory(source_directory):
+    """
+    Removes unwanted .heic files from the specified source directory.
+    Args:
+        source_directory (str): The path to the source directory.
+        unwanted_file_names (list): A list of unwanted .heic file names to be removed.
+    """
+    for root, dirs, files in os.walk(source_directory):
+        for file in files:
+            if file.lower().endswith(".heic"):
+                os.remove(os.path.join(root, file))
+
+
+######################################################
+###                 Start Skript                   ###
+######################################################
+source = "/app/skripts/image_folders/images_source"
+heic_files = create_list_of_heic_files(source)
+for file in heic_files:
+    convert_heic_to_jpeg(file, source)
+# remove_unwanted_heic_files_from_directory(source)
+print(os.listdir(source))
